aa5e715fcb6ca5265c1776492f2ca6ab8e939be5
FeMotta <fernandosantosmotta@gmail.com>
2023-03-31 19:32:11 -0300

    remove arquivos

--- api/index.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-const app = express();
-const port = process.env.PORT || 3001;
-
-
-app.listen(port, () => {
-  console.log(`Servidor intermediário executando na porta ${port}`);
-});
