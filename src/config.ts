2611ba94fe30595ec08804d3df7add6e04cef507
FeMotta <fernandosantosmotta@gmail.com>
2023-04-23 12:47:04 -0300

    adicioina url para cors

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,7 @@ const app = express();
 
 const port: number = Number(process.env.PORT);
 const host: string = process.env.HOST || 'localhost';
-const corsOrigin: string = process.env.CORS_ORIGIN || '';
+const corsOrigin: string = process.env.CORS_ORIGIN || 'https://proud-grass-031684c10.3.azurestaticapps.net/';
 
 app.use(cors({ origin: corsOrigin }));
 app.use(express.json());
