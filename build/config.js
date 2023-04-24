122474c9eb0925efab18e5a8375ea76ef7fbffe6
FeMotta <fernandosantosmotta@gmail.com>
2023-04-24 14:55:54 -0300

    deploy 3

--- build/config.js
+++ build/config.js
@@ -1,20 +1,40 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.host = exports.port = exports.app = void 0;
-const express_1 = __importDefault(require("express"));
-const cors_1 = __importDefault(require("cors"));
-const dotenv_1 = __importDefault(require("dotenv"));
-dotenv_1.default.config();
-const app = (0, express_1.default)();
+const express = __importStar(require("express"));
+const cors = __importStar(require("cors"));
+const dotenv = __importStar(require("dotenv"));
+dotenv.config();
+const app = express();
 exports.app = app;
 const port = Number(process.env.PORT) || 3001;
 exports.port = port;
 const host = process.env.HOST || 'localhost';
 exports.host = host;
 const corsOrigin = process.env.CORS_ORIGIN || 'https://react-smartai.ue.r.appspot.com';
-app.use((0, cors_1.default)({ origin: corsOrigin }));
-app.use(express_1.default.json());
+app.use(cors({ origin: corsOrigin }));
+app.use(express.json());
 //# sourceMappingURL=config.js.map
