47ebac1b95df4be7facbdd9560b9ea15ebeaf0e2
FeMotta <fernandosantosmotta@gmail.com>
2023-04-24 14:37:52 -0300

    adiciona get

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,12 @@ import { logger } from './logger';
 app.use('/api', generateQuestionRouter);
 app.use('/api', checkAnswerRouter);
 
+app.get('/', (req, res) => {
+  res.json({
+    message: 'SmartAI API',
+  })
+});
+
 app.listen(port, host, () => {
   logger.info(`Server running at http://${host}:${port}`);
 });
